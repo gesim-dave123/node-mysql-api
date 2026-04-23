da5b36b3ceeba8fdeda5e2bba7f7cb2a8e7b3677
gesim-dave123 <christiandavegesim@gmail.com>
2026-04-23 14:49:03 +0800

    Created accounts model

---
+++ src/models/account.model.ts
@@ -0,0 +1,28 @@
+import {DataTypes} from 'sequelize';
+
+export default function accountModel(sequelize: any){
+    const attributes = {
+        email: {type: DataTypes.STRING, allowNull: false},
+        passwordHash: {type: DataTypes.STRING, allowNull: false},
+        firstName: {type: DataTypes.STRING, allowNull: false},
+        lastName: {type: DataTypes.STRING, allowNull: false},
+        role: {type: DataTypes.STRING, allowNull: false},
+        acceptTerms: {type: DataTypes.BOOLEAN, allowNull: false},
+        verificationToken: {type: DataTypes.STRING},
+        verified: {type: DataTypes.DATE},
+        resetToken: {type: DataTypes.STRING},
+        passwordReset: {type: DataTypes.DATE},
+        created: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
+        updated: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
+        isVerified: {type: DataTypes.VIRTUAL, get() {return !!(this.verified || this.passwordReset )}},
+
+    };
+    const options = {
+        timestamps: false,
+        defaultScope: {
+            attributes: {exclude: ['passwordHash']}
+        },
+        scopes: {withHash: {attributes: {}, include: ['passwordHash']}}
+    };
+    return sequelize.define('account', attributes, options);
+}
