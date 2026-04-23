e833d43958ade71e356d90ca1514c122668bc24c
gesim-dave123 <christiandavegesim@gmail.com>
2026-04-23 14:46:58 +0800

    Implemented authorize function middlewre

---
+++ src/_middleware/authorize.ts
@@ -0,0 +1,29 @@
+import { expressjwt as jwt } from "express-jwt";
+import { Request, Response } from "express";
+import db from "../_helpers/db";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default function authorize(roles: any = []) {
+  if (typeof roles === "string") {
+    roles = [roles];
+  }
+
+  return [
+    jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
+    async (req: any, res: any, next: any) => {
+      const account = await db.Account.findByPk(req.user.id);
+
+      if (!account || (roles.length && !roles.includes(account.role))) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      req.user.role = account.role;
+      const refreshToken = await db.getRefreshToken();
+      req.user.ownsToken = (token: any) =>
+        !!refreshToken.find((x: any) => x.token === token);
+      next();
+    },
+  ];
+}
