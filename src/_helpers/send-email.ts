b711500bbd2c49553f302749fe083673b6b53847
gesim-dave123 <christiandavegesim@gmail.com>
2026-05-21 04:03:11 +0800

    Added resend overide

--- src/_helpers/send-email.ts
+++ src/_helpers/send-email.ts
@@ -7,9 +7,10 @@ dotenv.config();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
+    const recipient = process.env.RESEND_OVERRIDE_TO || to;
     const { data, error } = await resend.emails.send({
         from: from,
-        to: [to],
+        to: [recipient],
         subject: subject,
         html: html,
     });
